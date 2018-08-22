8907e6f9c07888dcb4c5a5676e98e9d6020fcb53
faustomontiel <36174205+faustomontiel@users.noreply.github.com>
2018-08-21 22:11:04 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num=0,cubo=0,cuadrado=0;
+            int potencia = 3;
+            int poitenciaCuad = 2;
+         
+            
+               Console.WriteLine("Introduce un número:");
+               num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+
+                cubo = Math.Pow(num, potencia);
+                cuadrado = Math.Pow(num, poitenciaCuad);
+            }
+            else
+            {
+                Console.WriteLine("ERROR!");
+            }
+            Console.WriteLine("Su cubo es  " + cubo);
+            Console.WriteLine("Su cuadrado es  " + cuadrado);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0,i, num;
+            Console.WriteLine("Ingrese numero");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = 0; j < num; j++)
+            {
+
+                for ( i = 1; i < (num+1) ; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        a++;
+                    }
+                }
+
+                if (a != 2)
+                {
+                    Console.WriteLine(" "+j);
+                }
+                else
+                {
+                    
+                   Console.WriteLine(" "+i );
+                }
+             }
+                Console.ReadLine();
+        }
+    }
+
+  }
+
+
+
