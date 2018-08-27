9e17034055e4218a09eedc569f4251160e67c62e
faustomontiel <36174205+faustomontiel@users.noreply.github.com>
2018-08-26 23:52:05 -0300

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, anio, d, m, a;
+
+            Console.Write("CALCULAR EDAD");
+            Console.Write("Ingrese dia: ");
+            int.TryParse(Console.ReadLine(), out dia);
+
+            Console.Write("Ingrese mes: ");
+            int.TryParse(Console.ReadLine(), out mes);
+
+            Console.Write("Ingrese año: ");
+            int.TryParse(Console.ReadLine(), out anio);
+
+            a = DateTime.Now.Year - anio;
+            m = DateTime.Now.Month - mes;
+            d = DateTime.Now.Day - dia;
+
+            Console.WriteLine("Su edad es: " + a + " años, " + m + " meses, " + d + " días");
+            Console.ReadKey();
+        }
+    }
+}
