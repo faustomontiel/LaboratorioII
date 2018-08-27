969cfa7ac8a7ff35523de61f6ec27cd8e5e84168
faustomontiel <36174205+faustomontiel@users.noreply.github.com>
2018-08-26 23:28:23 -0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio;
+            Console.WriteLine("Ingrese año: ");
+            while (!int.TryParse(Console.ReadLine(), out anio))
+            {
+                Console.WriteLine("Error. Ingrese un año valido.");
+            }
+            if (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0))
+            {
+                Console.WriteLine("El año " + anio + " Si es bisiesto ");
+            }
+            else
+            {
+                Console.WriteLine("El año " + anio + " No es bisiesto ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
