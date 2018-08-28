0bcd05b3e05c754815359e03d6f540993ccdbe0e
faustomontiel <36174205+faustomontiel@users.noreply.github.com>
2018-08-27 21:39:59 -0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int max = 0;
+            int min = 0;
+            bool flag = false;
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Ingrese un numero entre -100 y 100");
+                int.TryParse(Console.ReadLine(), out numero);
+
+                if (!(Validacion.Validar(numero, 100, -100)))
+                {
+                    Console.WriteLine("Se exedio el limite");
+
+                }
+                else
+                {
+                    if (numero < min || flag == false)
+                    {
+                        min = numero;
+                        flag = true;
+                    }
+
+                    if (numero > max || flag == false)
+                    {
+                        max = numero;
+                        flag = true;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("El menor numero es : " + min);
+            Console.WriteLine("El mayor numero es : " + max);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp11/ConsoleApp11/validacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    /// <summary>
+    /// Vailida que no se exeda el limite.
+    /// </summary>
+    public class Validacion
+    {
+
+        public static bool Validar(int valor,int maximo,int minimo)
+        {
+            bool flag=false;
+
+            if(valor < maximo && valor > minimo)
+            {
+                flag = true;
+            }
+            return flag;
+        }
+
+
+    }
+}
